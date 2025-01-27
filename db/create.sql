7fa68714f27e42a7e722af3603e14fbc58d713f3
AlexMoller113151 <113151@emmauscollege.nl>
2025-01-27 11:28:31 +0000

    aanpassingen Samen les 1

--- db/create.sql
+++ db/create.sql
@@ -20,10 +20,10 @@ CREATE TABLE products (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price) values ('mercedes amg 1', 'De duurste mercedes op de markt.', '816905633-0', 3500000);
-insert into products (name, description, code, price) values ('BMW XM', 'BM dikste SUV Samons favoriet.', '077030122-3', 200000);
-insert into products (name, description, code, price) values ('Scarlet and the Black, The', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 13.5);
-insert into products (name, description, code, price) values ('Aquí llega Condemor, el pecador de la pradera', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 13.5);
-insert into products (name, description, code, price) values ('Kiss for Corliss, A (Almost a Bride)', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
-insert into products (name, description, code, price) values ('Velvet Goldmine', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14);
+insert into products (name, description, code, price) values ('Renault E space sport', 'De mooist elektrice op de markt.', '816905633-0', 49000);
+insert into products (name, description, code, price) values ('BMW XM', 'BMW dikste SUV Samons favoriet.', '077030122-3', 210000);
+insert into products (name, description, code, price) values ('Mercedes gle 63s amg' 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 270000);
+insert into products (name, description, code, price) values ('Range Rover Sport S' 'betaalbare luxe SUV met een enorme kracht vermogen.', '693155505-7', 140000);
+insert into products (name, description, code, price) values ('Audi RS Q8', 'sport SUV net enorme kracht en een vurige look.', '686928463-6', 262000);
+insert into products (name, description, code, price) values ('Golf GTI club sport', '  Middenklasse auto, enorm kracht voor een kleine auto. Blaast weg', '492662523-7', 70000);
 
