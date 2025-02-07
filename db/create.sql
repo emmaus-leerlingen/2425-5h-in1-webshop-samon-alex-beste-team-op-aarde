3e09a6e4f02982830f40d35734141761403b33e2
AlexMoller113151 <113151@emmauscollege.nl>
2025-02-07 10:41:15 +0000

    alle foto's aangepast (alex)

--- db/create.sql
+++ db/create.sql
@@ -22,8 +22,8 @@ CREATE TABLE products (
 
 insert into products (name, description, code, price) values ('Renault E space sport', 'De mooist elektrice op de markt.', '816905633-0', 49000);
 insert into products (name, description, code, price) values ('BMW XM', 'BMW dikste SUV Samons favoriet.', '077030122-3', 210000);
-insert into products (name, description, code, price) values ('Mercedes gle 63s amg' 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 270000);
-insert into products (name, description, code, price) values ('Range Rover Sport S' 'betaalbare luxe SUV met een enorme kracht vermogen.', '693155505-7', 140000);
+insert into products (name, description, code, price) values ('Mercedes gle 63s amg', 'Mooiste mercedes op de markt volgens Alex.', '445924201-X', 270000);
+insert into products (name, description, code, price) values ('Range Rover Sport S' ,'betaalbare luxe SUV met een enorme kracht vermogen.', '693155505-7', 140000);
 insert into products (name, description, code, price) values ('Audi RS Q8', 'sport SUV net enorme kracht en een vurige look.', '686928463-6', 262000);
-insert into products (name, description, code, price) values ('Golf GTI club sport', '  Middenklasse auto, enorm kracht voor een kleine auto. Blaast weg', '492662523-7', 70000);
+insert into products (name, description, code, price) values ('Golf GTI club sport', ' Middenklasse auto, enorm kracht voor een kleine auto. Blaast weg', '492662523-7', 70000);
 
